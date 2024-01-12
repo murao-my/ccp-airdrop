ec5e83a2389dd70c1dd1543bd7ac4c9675e3b779
Lideru <LideruBcgDev@gmail.com>
2024-01-13 02:15:47 +0900

    エアドロインターバルは0、1groupあたり3000アドレスに変更

--- src/config.py
+++ src/config.py
@@ -40,14 +40,14 @@ def startAddressIndex():
     group = int(os.environ.get("group", 1))
     start = int(os.environ.get("startAddressIndex", -1))
     if start == -1:
-        return (group - 1) * 1200 + 1
+        return (group - 1) * 3000 + 1
     return start
 
 def maxAddressIndex():
     group = int(os.environ.get("group", 1))
     max = int(os.environ.get("maxAddressIndex", -1))
     if max == -1:
-        return group * 1200
+        return group * 3000
     return max
 
 def airdropInterval():
